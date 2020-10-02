8eb84cb2c3ec94ad9a2858d997788ad4060c8c1b
guptarahul15 <rahulgupta@rogers.com>
2020-10-02 14:46:41 -0400

    set up components

---
+++ src/components/CircleSelector/CircleSelector.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+const CircleSelector = (props) => <div className="component">CircleSelector</div>;
+
+export default CircleSelector;
---
+++ src/components/Circles/Circles.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+const Circles = (props) => <div className="component">Circles</div>;
+
+export default Circles;
