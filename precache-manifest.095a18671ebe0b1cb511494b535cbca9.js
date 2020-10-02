fe6e6a7344b63e566ec9ac25f42479ad52a1985d
guptarahul15 <rahulgupta@rogers.com>
2020-10-02 18:55:37 -0400

    Updates

---
+++ precache-manifest.095a18671ebe0b1cb511494b535cbca9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6e74dd4a55197e743e83d5e681b41c0",
+    "url": "/project-4-assessment/index.html"
+  },
+  {
+    "revision": "096c910eabaabbcf667e",
+    "url": "/project-4-assessment/static/css/main.3da10506.chunk.css"
+  },
+  {
+    "revision": "acc314c7a357911f8cd2",
+    "url": "/project-4-assessment/static/js/2.0d68efe0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/project-4-assessment/static/js/2.0d68efe0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "096c910eabaabbcf667e",
+    "url": "/project-4-assessment/static/js/main.a28def6f.chunk.js"
+  },
+  {
+    "revision": "5f6f705e1e3b3b948cc9",
+    "url": "/project-4-assessment/static/js/runtime-main.3bb3fb77.js"
+  },
+  {
+    "revision": "8a684df1c030b1d96c9faaafce455c19",
+    "url": "/project-4-assessment/static/media/psyduck.8a684df1.png"
+  }
+]);
